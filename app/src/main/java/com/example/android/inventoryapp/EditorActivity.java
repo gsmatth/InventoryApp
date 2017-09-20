3d8f9ffa5ce39d51a5451b846c56d84cd4ca87a2
Doug Popadince <gsmatth@icloud.com>
2017-09-20 16:30:18 -0700

    added project scaffolding

---
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp;
+
+/**
+ * Created by djp on 9/20/17.
+ */
+
+public class EditorActivity {
+}
---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp.data;
+
+/**
+ * Created by djp on 9/20/17.
+ */
+
+public class ProductDbHelper {
+}
