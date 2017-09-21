55e66bbd7ee7dbf4ae726651d3e43f649a4074f2
Doug Popadince <gsmatth@icloud.com>
2017-09-21 12:04:49 -0700

    added ProductContract

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -3,11 +3,11 @@ package com.example.android.inventoryapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProductActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        setContentView(R.layout.activity_inventory);
     }
 }
