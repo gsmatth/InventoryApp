03a0328c6021bdb563fba4bc2f04e4da4caf9e91
Doug Popadince <gsmatth@icloud.com>
2017-09-22 14:39:39 -0700

    added basic functionality for ProductCursorAdapater

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,13 +1,37 @@
 package com.example.android.inventoryapp;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class InventoryActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public static final String LOG_TAG = InventoryActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        ListView productItems = (ListView) findViewById(R.id.product_listview);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
--- app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -1,8 +1,49 @@
 package com.example.android.inventoryapp;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
 /**
  * Created by djp on 9/20/17.
  */
 
-public class ProductCursorAdapter {
+public class ProductCursorAdapter  extends CursorAdapter{
+    public ProductCursorAdapter(Context context, Cursor cursor){
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.item_product, parent, false);
+    }
+
+    /**
+     * This method binds the product data(in the row being pointed to in the cursor)
+     * to the given list item layout.  For example, the name of the current product can be set on
+     * the "item_product_name" text view in the list item layout
+     * @param view          existing view
+     * @param context       app context
+     * @param cursor        The cursor from which to get the data.  The cursoor is already  moved
+     *                      to the correct row
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView productName = (TextView) view.findViewById(R.id.item_product_name);
+        TextView productQuantity = (TextView) view.findViewById(R.id.item_product_quantity);
+        TextView productPrice = (TextView) view.findViewById(R.id.item_product_price);
+
+        String name = cursor.getString(cursor.getColumnIndex("name"));
+        String quantity = cursor.getString(cursor.getColumnIndex("quantity"));
+        String price = cursor.getString(cursor.getColumnIndex("price"));
+
+        productName.setText(name);
+        productQuantity.setText(quantity);
+        productPrice.setText(price);
+
+    }
 }
